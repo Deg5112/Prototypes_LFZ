29b662e2fc3feeb039fd5385bd97469cf2f4770d
David Goodman <deg5112@gmail.com>
2015-11-03 18:51:56 -0800

    finished with ajax proto

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -1,5 +1,6 @@
 //Create GLOBAL variable below here on line 2
 
+var global_result;
 
 $(document).ready(function(){
 	$('button').click(function(){
@@ -9,7 +10,16 @@ $(document).ready(function(){
 			url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
 			success: function(result) {
 				console.log('AJAX Success function called, with the following result:', result);
+				global_result = result.feed.entry;//returns node list of all objects
+				for(var i = 0; i <global_result.length;i++){
+					var curSrc = global_result[i]['im:image'][2].label;
+					var newImage = $('<img>').attr('src', curSrc);
 
+					var title = global_result[i].title.label;
+					var figCaption = $('<figcaption>').text(title);
+					$('#main').append(newImage, figCaption);
+				}
+				console.log(global_result);
 			}
 		});
 		console.log('End of click function');
