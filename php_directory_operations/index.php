e7f8588ee3ada4033da6ccc8c190c4c11bb1489b
David Goodman <deg5112@gmail.com>
2015-11-29 12:21:21 -0500

    done with carousel.. need to do the rest of the features

--- php_directory_operations/index.php
+++ php_directory_operations/index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE>
 <html>
 <head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="jquery-2.1.4.min.js"></script>
 <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">-->
 
 <!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">-->
 
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>-->
 <style>
     div#image-container {
         border: 1px solid black;
@@ -36,7 +36,6 @@
     img {
         width: 100%;
         height: 100%;
-        display: inline;
     }
     #next, #prev {
         background-color: rgba(58, 51, 51, 0.23);
@@ -54,8 +53,9 @@
         left: 0;
         bottom: 50%;
     }
-    .glyphicon{
-        padding: 13px;
+    .glyphicon {
+        padding: 23px;
+        background-color: gray;
     }
 </style>
 </head>
--- php_directory_operations/object.php
+++ php_directory_operations/object.php
@@ -2,25 +2,67 @@
     $(function(){
         var carousel1 = new carousel();
         carousel1.init();
-
+        $('.glyphicon.glyphicon-chevron-right').click(function(){
+           carousel1.next(carousel1.imgArray);
+        });
+        $('.glyphicon.glyphicon-chevron-left').click(function(){
+           carousel1.prev(carousel1.imgArray);
+        });
     });
+
 var carousel = function() {
     var self = this;
     this.imgArray = [];
+    this.index = 0;
+    this.slideInterval = null;
     this.init = function(){
         self.getPhotos();
     };
     //next
-    this.next = function () {
-
+    this.next = function (array) {
+        clearInterval(self.slideInterval);
+        self.index+=1;
+        if(self.index === array.length) {
+            self.index = 0;
+        }
+        $('.image').remove();
+        var startImg = self.imgArray[self.index][0];
+        var $innerContainer = $('#inner-container');
+        $innerContainer.append(startImg);
+        self.slideShow(self.imgArray);
     };
 
     //prev
-    this.prev = function () {
-
+    this.prev = function (array) {
+        clearInterval(self.slideInterval);
+        self.index-=1;
+        if(self.index < 0) {
+            self.index = array.length-1;
+        }
+        $('.image').remove();
+        var startImg = self.imgArray[self.index][0];
+        var $innerContainer = $('#inner-container');
+        $innerContainer.append(startImg);
+        self.slideShow(self.imgArray);
     };
 
-//    //ajax to get photos on dom ready.
+    this.slideShow = function(array){
+
+        var $innerContainer = $('#inner-container');
+
+        self.slideInterval = setInterval(function() {
+            self.index += 1;
+            if(self.index === array.length) {
+                self.index = 0;
+            }
+            var nextImg = array[self.index][0];
+            $('.image').remove();
+            $innerContainer.append(nextImg).hide().fadeIn('slow');
+
+        }, 3000);
+    };
+
+    //ajax to get photos on dom ready.
     this.getPhotos = function () {
         $.ajax({
             dataType: 'json',
@@ -31,21 +73,14 @@ var carousel = function() {
                     var img = $('<img>').attr('src', response.images[i]).addClass('image');
                     self.imgArray.push(img);
                 }
-                var startImg = self.imgArray[0][0];
+                var startImg = self.imgArray[self.index][0];
                 var $innerContainer = $('#inner-container');
                 $innerContainer.append(startImg);
-                var index = 0;
-                setTimeout(function(){
-                    index+=1;
-                    //if index is equal to array length
-                    //set index back to 0..
-                    //remove the inner picture..
-                    //replace with new picture..
-                    //and so on..
-                        }
-                }, 2000);
+              self.slideShow(self.imgArray);
             }
+
         });
+
     };
 };
 
