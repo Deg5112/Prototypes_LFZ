fb5f71eec9e1fd64592041397342aad4b2a17763
David Goodman <deg5112@gmail.com>
2015-12-01 16:55:46 -0800

    update

---
+++ php-sessions/session_reader.php
@@ -0,0 +1,38 @@
+<!-- Session Reader -->
+<?php
+
+$_SESSION['form-data'] = $_POST;
+
+$name = $_POST["name"];
+$age = $_POST["age"];
+$occupation = $_POST['occupation'];
+
+                //regex patt      test string
+if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+    $nameErr = "Only letters and white space allowed";
+    $_SESSION['error']['name'] = $nameErr;
+}
+
+if (!preg_match("/^[a-zA-Z ]*$/",$occupation)) {
+    $occupationErr = "Only letters and white space allowed";
+    $_SESSION['error']['occupation'] = $occupationErr;
+}
+
+if (!preg_match("/^[0-9]+$/", $age)) {
+    $ageErr = 'only numbers allowed';
+    $_SESSION['error']['age'] = $ageErr;
+}
+
+
+if(isset($_SESSION['error'])){
+    header('location: session_setter.php');
+}
+//if any errors.. return to original page..
+
+//otherwise..
+//print_r($_SESSION);
+
+
+
+?>
+
---
+++ php-sessions/session_setter.php
@@ -0,0 +1,7 @@
+<!-- Session Setter -->
+<form action="session_reader.php" method="post">
+    <input type="text" name="name" placeholder="name">
+    <input type="text" name="age" placeholder="age">
+    <input type="text" name="occupation" placeholder="occupation">
+    <button type="submit">Submit</button>
+</form>
