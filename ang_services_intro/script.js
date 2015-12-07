2582fda3ab72bace8d94f4fc6602c98cc8e8c7e5
David Goodman <deg5112@gmail.com>
2015-12-07 11:40:58 -0800

    done

--- ang_services_intro/script.js
+++ ang_services_intro/script.js
@@ -5,28 +5,21 @@ app.controller('getMovies', function($http, $log, $scope){
 
     self.message = 'hello';
     self.constructUrl = function(query, self){
-
         var queryString = 'https://itunes.apple.com/search?term=' +  query    + '&callback=JSON_CALLBACK';
 
         self.getData(queryString);
     };
     self.getData = function(url){
         console.log(url);
+        $http.jsonp(url).then(function(response){
+            $log.info(response.data);
+            $log.info(response.data.results[0].artworkUrl30);
 
-        $http({
-            url: url,
-            dataType: 'json',
-            method: 'POST',
-            cache: false
-        }).then(function(response){
-                $scope.response = (response);
+            var img = $('<img>').attr('src', response.data.results[0].artworkUrl30);
+            $log.log(img);
+            $('body').append(img);
             }, function(response){
-                $scope.response = (response);
         });
     };
 });
-// .controller('logController', function($scope, $log){
-//    $scope.$log = $log;
-//    $scope.$message = $scope.response;
-//});
 
