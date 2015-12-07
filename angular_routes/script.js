fda6d15294e255427d698ef7dd53659c4b2ca337
David Goodman <deg5112@gmail.com>
2015-12-06 17:59:25 -0800

    ang_routes

---
+++ angular_routes/script.js
@@ -0,0 +1,27 @@
+// Create the route module and name it routeApp
+var app = angular.module('routeApp');
+
+app.controller('routeCtrl', function($scope){
+    $scope.message: 'lorem ipsum'
+});
+
+// Config the routes
+
+         // route for the home page
+
+        // route for the about page
+
+        // route for the contact page
+
+
+// Create the controllers for the different pages below
+
+// home page controller
+    // Create a message to display in the view
+
+
+// about page controller
+    // Create a message to display in the view
+
+// contact page controller
+    // Create a message to display in the view
